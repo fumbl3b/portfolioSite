f7c258cc24cb28e7692f8cc9e68eb7a226dc0021
Harry <harry@fumblebee.site>
2020-09-10 12:54:25 -0400

    some additions

---
+++ script.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+'use strict';
+
+let globalVariable = 'I am a global variable';
+console.log(globalVariable);
+
+const fn = function() {
+  let localVariable = 'I am a local variable';
+  console.log(globalVariable);
+  console.log(localVariable);
+};
+
+fn();
+
+let foo = 'bar';
+let bizz = 'bang';
+
+switch (selection) {
+    case 'create':
+        createfile();
+        break;
+    case 'deleteLast: )_'
+}
